4cbb767c441170ead60d3be1c9c52c9e79b6b4af
anettgor <anetagorowska06@onet.pl>
2022-12-05 20:47:11 +0100

    task 2 completed

--- JS/task-02.js
+++ JS/task-02.js
@@ -7,24 +7,11 @@ const ingredients = [
   "Condiments",
 ];
 
-const test = document.createElement("li");
-const ingridientsTag = document.getElementById("ingridients");
+const ingredientsId = document.querySelector("#ingredients");
 
-// ingredients.forEach(ingridient ==> {
-
-// }
-// )
-
-test.textContent = "This is a li";
-console.log(ingridientsTag.append(test));
-
-// const heading = .createElement("h1");
-// console.log(heading); // <h1></h1>
-
-// heading.textContent = "This is a heading";
-// console.log(heading); // <h1>This is a heading</h1>
-
-// const image = document.createElement("img");
-// image.src = "https://placeimg.com/640/480/nature";
-// image.alt = "Nature";
-// console.log(image); // <img src="https://placeimg.com/640/480/nature" alt="Nature" />
+ingredients.forEach((ingredient) => {
+  const newTag = document.createElement("li");
+  newTag.textContent = ingredient;
+  newTag.classList.add("item");
+  ingredientsId.append(newTag);
+});
