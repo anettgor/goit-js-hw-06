f0a28fc82a5fada10b2b7dccae8a26ea7f208015
anettgor <anetagorowska06@onet.pl>
2022-12-10 16:00:43 +0100

    task 9 completed

--- JS/task-09.js
+++ JS/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColor = document.querySelector(".change-color");
+const colorHex = document.querySelector(".color");
+
+changeColor.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  colorHex.textContent = randomColor;
+});
