17f1de8bf784d9db9eaf6c63840ac7d72f9bcf92
anettgor <anetagorowska06@onet.pl>
2022-12-10 15:00:01 +0100

    task 8 completed

--- JS/task-08.js
+++ JS/task-08.js
@@ -1,1 +1,17 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("Uzupełnij wszystkie pola");
+  } else {
+    const loginObject = { email: email.value, password: password.value };
+    console.log(loginObject);
+    event.currentTarget.reset();
+  }
+});
