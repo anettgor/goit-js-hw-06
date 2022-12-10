e2123321645ce416123b6949f2b6a50cd99e58a8
anettgor <anetagorowska06@onet.pl>
2022-12-10 19:56:02 +0100

    task 10 completed

--- JS/task-10.js
+++ JS/task-10.js
@@ -3,3 +3,39 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+let initialValue = 30;
+
+const boxes = document.querySelector("#boxes");
+const input = document.querySelector("input");
+const destroyBtn = document.querySelector("[data-destroy]");
+const createBtn = document.querySelector("[data-create]");
+
+const createBoxes = (amount) => {
+  for (let i = 0; i < amount; i++) {
+    const newDivs = document.createElement("div");
+    Object.assign(newDivs.style, {
+      backgroundColor: getRandomHexColor(),
+      height: `${initialValue}px`,
+      width: `${initialValue}px`,
+    });
+    // alternative option for Object.assign
+    //newDivs.style.backgroundColor = getrandomHexColor()
+    //newDivs.style.height = `${initialValue}px`
+    //newDivs.style.width = `${initialValue}px`
+    initialValue += 10;
+    boxes.appendChild(newDivs);
+  }
+};
+
+const destroyBoxes = () => {
+  initialValue = 30;
+  boxes.innerHTML = "";
+  input.value = "";
+};
+
+createBtn.addEventListener("click", () => {
+  createBoxes(input.value);
+});
+
+destroyBtn.addEventListener("click", destroyBoxes);
